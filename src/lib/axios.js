26ae491eb7fb2978c94bc9f6561a5435ec22c703
Rizky Wahyu Prasetiyo <rizkywahyupc@gmail.com>
2023-06-11 23:01:24 +0700

    mengembalikan ke asal

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const axios = Axios.create({
-    baseURL: 'https://belajar-api.simelek.com',
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
